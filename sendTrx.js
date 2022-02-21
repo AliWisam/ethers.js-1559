03f586c38fb3fd419d6aaedbeee8f5690a364aab
Ali Wisam <aliwisaam73@gmail.com>
2022-02-21 15:40:10 +0500

    Update sendTrx.js

--- sendTrx.js
+++ sendTrx.js
@@ -8,10 +8,29 @@
 
 
  const gRole = await nftMarket.grantTokenListRole(addr 
-        ,{
-          "maxFeePerGas":feeData?.maxFeePerGas,
-          "maxPriorityFeePerGas":feeData?.maxPriorityFeePerGas
-         
-        });
+ ,{
+  "maxFeePerGas":feeData?.maxFeePerGas,
+  "maxPriorityFeePerGas":feeData?.maxPriorityFeePerGas  
+  });
+
+##Using PolyGon Gas Station
+
+ const response = await fetch('https://gasstation-mainnet.matic.network/v2');
+ const { fast } = await response.json()
+ console.log("Gas station data", fast);
+ console.log("Gas station data", fast.maxFee, fast.maxPriorityFee)
+ console.log("GASS", ethers.utils.parseUnits(`${fast?.maxFee.toFixed(2)}`, "gwei"))
+
+  let transaction = await contract.createToken(url,
+  {
+   maxFeePerGas: ethers.utils.parseUnits(`${fast?.maxFee.toFixed(2)}`, "gwei"),
+   maxPriorityFeePerGas: ethers.utils.parseUnits(`${fast?.maxPriorityFee.toFixed(2)}`, "gwei")
+   });
+  let tx = await transaction.wait();
+  let event = tx.events[0];
+
+  let value = event.args[2];
+  let tokenId = value.toNumber();
+  console.log('tokenId', tokenId);
         
         
